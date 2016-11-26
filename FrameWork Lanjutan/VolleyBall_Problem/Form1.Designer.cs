2e7a8e4c891fb076ec3e636d6965efe09687c01d
myevil <williamkarno@gmail.com>
2016-11-26 20:33:38 +0700

    VolleyBall

---
+++ FrameWork Lanjutan/VolleyBall_Problem/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace VolleyBall_Problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.btnHitung = new System.Windows.Forms.Button();
+            this.TxtHasil = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(23, 12);
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(100, 22);
+            this.Txt1.TabIndex = 0;
+            this.Txt1.Text = "1";
+            // 
+            // Txt2
+            // 
+            this.Txt2.Location = new System.Drawing.Point(23, 40);
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(100, 22);
+            this.Txt2.TabIndex = 1;
+            this.Txt2.Text = "2";
+            // 
+            // btnHitung
+            // 
+            this.btnHitung.Location = new System.Drawing.Point(23, 68);
+            this.btnHitung.Name = "btnHitung";
+            this.btnHitung.Size = new System.Drawing.Size(75, 23);
+            this.btnHitung.TabIndex = 2;
+            this.btnHitung.Text = "button1";
+            this.btnHitung.UseVisualStyleBackColor = true;
+            this.btnHitung.Click += new System.EventHandler(this.btnHitung_Click_1);
+            // 
+            // TxtHasil
+            // 
+            this.TxtHasil.Location = new System.Drawing.Point(23, 107);
+            this.TxtHasil.Name = "TxtHasil";
+            this.TxtHasil.Size = new System.Drawing.Size(100, 22);
+            this.TxtHasil.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.TxtHasil);
+            this.Controls.Add(this.btnHitung);
+            this.Controls.Add(this.Txt2);
+            this.Controls.Add(this.Txt1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt1;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.Button btnHitung;
+        private System.Windows.Forms.TextBox TxtHasil;
+    }
+}
+
---
+++ FrameWork Lanjutan/VolleyBall_Problem/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolleyBall_Problem
+{
+        public partial class Form1 : Form
+    {
+
+        public int Mod = 1000000007;
+
+        public Form1()
+        {
+            InitializeComponent();
+            TxtHasil.Text = "";
+        }
+
+        private long PowerMod(long a, long power, long n)
+        {
+            long mod = 1;
+
+            while (power != 0)
+            {
+                if ((power & 1) == 1)
+                    mod = mod * a % n;
+
+                a = (a * a) % n;
+                power >>= 1;
+            }
+
+            return mod;
+        }
+
+        private long Volleyball(int a, int b)
+        {
+            int max = 25;
+            if (a > b) return Volleyball(b, a);
+            if (b < max) return 0;
+            if (b > max && b != a + 2) return 0;
+            if (b == max && b - a < 2) return 0;
+
+            long[,] value = new long[max, max];
+
+            for (int i = 0; i < max; i++)
+            {
+                for (int j = 0; j < max; j++)
+                {
+                    if (i == 0 || j == 0)
+                        value[i, j] = 1;
+                    else
+                        value[i, j] = (value[i - 1, j] + value[i, j - 1]) % Mod;
+                }
+            }
+
+            if (b == max)
+                return value[24, a];
+
+            return value[24, 24] % Mod * PowerMod(2, (a - 24), Mod) % Mod;
+        }
+
+        private void Txt1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Txt2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Txt1_KeyPress(sender, e);
+        }
+
+
+        private void btnHitung_Click_1(object sender, EventArgs e)
+        {
+            if (Txt1.Text == "")
+            {
+                MessageBox.Show("Anda harus memasukkan nilai akhir A", "Error");
+                return;
+            }
+            if (Txt2.Text == "")
+            {
+                MessageBox.Show("Anda harus memasukkan nilai akhir B", "Error");
+                return;
+            }
+            int a = Convert.ToInt32(Txt1.Text);
+            int b = Convert.ToInt32(Txt2.Text);
+            TxtHasil.Text = (Volleyball(a, b)).ToString();
+        }
+    }
+}
