e094737a15aa411bbe26a2bf13b91da9a1d19b43
myevil <williamkarno@gmail.com>
2016-12-30 23:41:23 +0700

    Tugas

---
+++ FrameWork Lanjutan/Latihan_Posting/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Latihan_Posting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtJumlah = new System.Windows.Forms.TextBox();
+            this.txtHarga = new System.Windows.Forms.TextBox();
+            this.Simpan = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.Keluar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nama Barang";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Jumlah";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Harga Jual";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(144, 8);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.TabIndex = 1;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(144, 34);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(328, 22);
+            this.txtNama.TabIndex = 1;
+            // 
+            // txtJumlah
+            // 
+            this.txtJumlah.Location = new System.Drawing.Point(144, 60);
+            this.txtJumlah.Name = "txtJumlah";
+            this.txtJumlah.Size = new System.Drawing.Size(100, 22);
+            this.txtJumlah.TabIndex = 1;
+            // 
+            // txtHarga
+            // 
+            this.txtHarga.Location = new System.Drawing.Point(144, 87);
+            this.txtHarga.Name = "txtHarga";
+            this.txtHarga.Size = new System.Drawing.Size(100, 22);
+            this.txtHarga.TabIndex = 1;
+            // 
+            // Simpan
+            // 
+            this.Simpan.Location = new System.Drawing.Point(16, 124);
+            this.Simpan.Name = "Simpan";
+            this.Simpan.Size = new System.Drawing.Size(75, 23);
+            this.Simpan.TabIndex = 2;
+            this.Simpan.Text = "Simpan";
+            this.Simpan.UseVisualStyleBackColor = true;
+            this.Simpan.Click += new System.EventHandler(this.Simpan_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(119, 124);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(75, 23);
+            this.Cancel.TabIndex = 2;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // Keluar
+            // 
+            this.Keluar.Location = new System.Drawing.Point(224, 124);
+            this.Keluar.Name = "Keluar";
+            this.Keluar.Size = new System.Drawing.Size(75, 23);
+            this.Keluar.TabIndex = 2;
+            this.Keluar.Text = "Keluar";
+            this.Keluar.UseVisualStyleBackColor = true;
+            this.Keluar.Click += new System.EventHandler(this.Keluar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 166);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(610, 92);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 271);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Keluar);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Simpan);
+            this.Controls.Add(this.txtHarga);
+            this.Controls.Add(this.txtJumlah);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtJumlah;
+        private System.Windows.Forms.TextBox txtHarga;
+        private System.Windows.Forms.Button Simpan;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Button Keluar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ FrameWork Lanjutan/Latihan_Posting/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+
+
+
+
+namespace Latihan_Posting
+{
+    public partial class Form1 : Form
+    {
+        string maks;
+        MySqlConnection connect = new MySqlConnection("Server=127.0.0.1;Database=pos;Uid=root;Pwd=12345");
+        DataTable oDT;
+        MySqlDataAdapter oDA;
+        MySqlCommand command;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        void Kosongkan()
+        {
+            command = new MySqlCommand("select ifnull(max(id),0)+1 from barang", connect);
+            oDA = new MySqlDataAdapter(command);
+            oDT = new DataTable();
+            oDA.Fill(oDT);
+            maks = oDT.Rows[0][0].ToString();
+            txtID.Text = oDT.Rows[0][0].ToString();
+            txtNama.Text = "";
+            txtJumlah.Text = "";
+            txtHarga.Text = "";
+        }
+        void Tampilsemua()
+        {
+            command = new MySqlCommand("select * from barang", connect);
+            oDA = new MySqlDataAdapter(command);
+            oDT = new DataTable();
+            oDA.Fill(oDT);
+
+            dataGridView1.DataSource = oDT;
+        }
+        void masukkanData()
+        {
+            try
+            {
+                connect.Open();
+                command = new MySqlCommand("insert into barang values(@id,@nama,@jumlahawal,@hargajual)", connect);
+                command.Parameters.AddWithValue("@id", txtID.Text);
+                command.Parameters.AddWithValue("@nama", txtNama.Text);
+                command.Parameters.AddWithValue("@jumlahawal", int.Parse(txtJumlah.Text));
+                command.Parameters.AddWithValue("@hargajual", Convert.ToDecimal(txtHarga.Text));
+                command.ExecuteNonQuery();
+                connect.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Tampilsemua();
+            Kosongkan();
+        }
+        void UbahData()
+        {
+            connect.Open();
+            try
+            {
+                command = new MySqlCommand("update barang set ,nama=@nama,jumlahawal=@jumlahawal,hargajual=@hargajual, where id=@id", connect);
+                command.Parameters.AddWithValue("@id", txtID.Text);
+                command.Parameters.AddWithValue("@nama", txtNama.Text);
+                command.Parameters.AddWithValue("@jumlahawal", int.Parse(txtJumlah.Text));
+                command.Parameters.AddWithValue("@hargajual", Convert.ToDecimal(txtHarga.Text));
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            Tampilsemua();
+            Kosongkan();
+            connect.Close();
+        }
+        private void Keluar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Kosongkan();
+        }
+
+        private void Simpan_Click(object sender, EventArgs e)
+        { 
+            if (txtID.Text == maks)
+            {
+                masukkanData();
+            }else
+            {
+                UbahData();
+            }
+        }
+
+    }
+}
